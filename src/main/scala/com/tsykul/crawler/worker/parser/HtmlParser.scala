736a15e94711108b0bb3884c986fd6bb78a70ca1
Konstantin Tsykulenko <tsykul@gmail.com>
2015-03-03 02:42:52 +0200

    Removed redundant code

--- src/main/scala/com/tsykul/crawler/worker/parser/HtmlParser.scala
+++ src/main/scala/com/tsykul/crawler/worker/parser/HtmlParser.scala
@@ -7,7 +7,6 @@ import scala.collection.JavaConversions._
 
 trait HtmlParser {
   def parseHtml(response: HttpResponse): List[String] = {
-    val elements = Jsoup.parse(response.entity.asString).select("a[href]").toList
-    elements.map(_.attr("href"))
+    Jsoup.parse(response.entity.asString).select("a[href]").toList.map(_.attr("href"))
   }
 }
